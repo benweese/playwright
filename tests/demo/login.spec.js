89d9d35aad5492d37a24d8f8369444c7538594eb
BenWeese <Ben.Weese@gmail.com>
2023-03-23 11:14:16 -0500

    init

---
+++ tests/demo/login.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@playwright/test");
+import { LoginPage } from "../../pages/login";
+
+test("test", async ({ page }) => {
+  const Login = new LoginPage(page);
+
+  await Login.goToLoginPage();
+  await Login.login("tomsmith", "SuperSecretPassword!");
+
+  // await page.goto("https://the-internet.herokuapp.com/login");
+  // await page.getByLabel("Username").click();
+  // await page.getByLabel("Username").fill("tomsmith");
+  // await page.getByLabel("Password").click();
+  // await page.getByLabel("Password").fill("SuperSecretPassword!");
+  // await page.getByRole("button", { name: " Login" }).click();
+});
