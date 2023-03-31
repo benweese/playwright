7b09b531ac819d17f3fc872182d658917ac4d2ec
BenWeese <Ben.Weese@gmail.com>
2023-03-31 11:03:22 -0500

    init 2

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -1,16 +1,44 @@
 const { test, expect } = require("@playwright/test");
 import { LoginPage } from "../../pages/login";
+require("dotenv").config();
+
+const baseUrl = "https://reqres.in/api";
+let token;
 
 test("test", async ({ page }) => {
   const Login = new LoginPage(page);
 
   await Login.goToLoginPage();
-  await Login.login("tomsmith", "SuperSecretPassword!");
+  await Login.login("tomsmith", process.env.S_PASS);
+});
 
-  // await page.goto("https://the-internet.herokuapp.com/login");
-  // await page.getByLabel("Username").click();
-  // await page.getByLabel("Username").fill("tomsmith");
-  // await page.getByLabel("Password").click();
-  // await page.getByLabel("Password").fill("SuperSecretPassword!");
-  // await page.getByRole("button", { name: " Login" }).click();
+test.describe.parallel("API Testing", () => {
+  test("Simple API test - assert response status", async ({ request }) => {
+    const response = await request.get(baseUrl + "/users/1");
+    expect(response.status()).toBe(200);
+
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+    expect(responseBody.data.id).toBe(1);
+    expect(responseBody.data.first_name).toBe("George");
+    expect(responseBody.data.last_name).toBe("Bluth");
+  });
+
+  test("Simple API test - assert invalid endpoint", async ({ request }) => {
+    const response = await request.get(baseUrl + "/users/invalid");
+    expect(response.status()).toBe(404);
+  });
+
+  test("POST Request - Login", async ({ request }) => {
+    const response = await request.post(baseUrl + "login", {
+      data: {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka",
+      },
+    });
+    const responseBody = JSON.parse(await response.text());
+    expect(response.status()).toBe(200);
+    expect(responseBody.token).toBeTruthy();
+    token = responseBody.token;
+  });
 });
